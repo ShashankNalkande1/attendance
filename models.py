c53566522a9354e4ffa7b0b23b9c3db200c62234
ShashankNalkande1 <shashank.nalkandecse2022@indoreinstitute.com>
2026-04-22 19:53:38 +0530

    fixed again

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set")
+     DATABASE_URL = "postgresg=require"
 
 # Engine configuration
 engine = create_engine(
